d6b88cb09549c1af17e28975030a6399bf92ebfe
Rafał Zaborowski <raffik2000@gmail.com>
2021-08-03 15:24:42 +0200

    refactoring the App comoponent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import './components/ExpenseItem';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -29,25 +27,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        date={expenses[0].date}
-        amount={expenses[0].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        date={expenses[1].date}
-        amount={expenses[1].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        date={expenses[2].date}
-        amount={expenses[2].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        date={expenses[3].date}
-        amount={expenses[3].amount}/>
+      <Expenses items={expenses}/>
     </div>
   );
 }
