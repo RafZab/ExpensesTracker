ec4cb14b7e40e3e476f5f5a7e70e17542f7c9d1d
Rafał Zaborowski <raffik2000@gmail.com>
2021-08-05 23:16:27 +0200

    adding a new expense to list it works

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,27 +13,15 @@ function Expenses(props) {
     return (
         <div>
             <Card className="expenses">
-                <ExpensesFilter selected={filteredYear} onChangeYear={filterChangeHandler}/>
-                <ExpenseItem
-                    title={props.items[0].title}
-                    date={props.items[0].date}
-                    amount={props.items[0].amount}
-                />
-                <ExpenseItem
-                    title={props.items[1].title}
-                    date={props.items[1].date}
-                    amount={props.items[1].amount}
-                />
-                <ExpenseItem
-                    title={props.items[2].title}
-                    date={props.items[2].date}
-                    amount={props.items[2].amount}
-                />
-                <ExpenseItem
-                    title={props.items[3].title}
-                    date={props.items[3].date}
-                    amount={props.items[3].amount}
-                />
+                <ExpensesFilter selected={filteredYear} onChangeYear={filterChangeHandler} />
+                {props.items.map(expense =>
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        date={expense.date}
+                        amount={expense.amount}
+                    />
+                )}
             </Card>
         </div>
     );
