be3874a5235f0aff65861221206bad3b9f0cbffd
Rafał Zaborowski <raffik2000@gmail.com>
2021-08-10 02:29:35 +0200

    hidding form behind button

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,34 @@
+import React, { useState } from 'react';
+
 import Card from '../UI/Card';
 import ExpenseFrom from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+
     const saveNewExpenseDateHandler = (enteredExpense) => {
         const newExpense = {
             ...enteredExpense,
             id: Math.random(),
         };
         props.onAddNewExpense(newExpense);
+        ChangeEditingHandler();
     };
+
+    const ChangeEditingHandler = () => {
+        setIsEditing((prevState) => !prevState);
+    };
+
+
+    const newExpensesContext = isEditing ?
+        <ExpenseFrom onCancel={ChangeEditingHandler} onSaveNewExpense={saveNewExpenseDateHandler} />
+        :
+        <button onClick={ChangeEditingHandler}>Add New Expense</button>
 
     return (
         <Card className="new-expense">
-            <ExpenseFrom onSaveNewExpense={saveNewExpenseDateHandler} />
+            {newExpensesContext}
         </Card>
     );
 };
